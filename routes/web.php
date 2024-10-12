337c8f0b187ee73b8f7a668b035700f046b681d2
soullcake <soullcakezz@gmail.com>
2024-10-12 10:36:09 -0300

    refactor: migra rotas web para rotas API
    faz a migracao das rotas do arquivo web.php para api.php

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use Illuminate\Auth\Events\Logout;
-
-Route::prefix('auth')->group(function () {
-    Route::get('/login', [AuthController::class, 'login_create'])->name('login_create');
-    Route::post('/login', [AuthController::class, 'login_store'])->name('login_store');
-
-    Route::get('/register', [AuthController::class, 'register_create'])->name('register_create');
-    Route::post('/register', [AuthController::class, 'register_store'])->name('register_store');
-
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-});
-
-// normal user
-Route::redirect('/', '/site/dashboard');
-
-Route::prefix('site')->middleware(['auth'])->group(function () {
-    Route::get('/dashboard', function() {
-        return view('site.dashboard');
-    });
-    
-});
